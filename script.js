c5f13ab927cde54f879517e6dbeff40bff37ab3a
Akash Gupta <124385823+Gupta-G-0406@users.noreply.github.com>
2026-01-19 18:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const form = document.getElementById("myForm");
 const nameInput = document.getElementById("name");
 const ageInput = document.getElementById("age");
-const submitBtn = document.getElementById("btn");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -9,20 +8,21 @@ form.addEventListener("submit", function (e) {
   const name = nameInput.value.trim();
   const age = parseInt(ageInput.value.trim());
 
-  if (name === "" || age === "") {
-    alert("Please enter valid details.");
+  if (name === "" || isNaN(age)) {
+    alert("Please enter valid details");
   } else {
     let promise = new Promise((resolve, reject) => {
       if (age > 18) {
         setTimeout(() => {
-          resolve("Welcome, . You can vote.");
+          resolve(`Welcome, ${name}. You can vote.`);
         }, 4000);
       } else {
         setTimeout(() => {
-          reject("Oh sorry . You aren't old enough.");
+          reject(`Oh sorry ${name}. You aren't old enough.`);
         }, 4000);
       }
     });
+
     promise
       .then((res) => {
         alert(res);
